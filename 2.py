8045842d90f5d7380a901a12a383392513128f7a
AlZurn <alex.zurnachyan@vk.com>
2021-09-05 11:32:41 +0300

    porc

---
+++ 2.py
@@ -0,0 +1,52 @@
+# import random
+# a = ['a''b''d''s''as']
+# c = []
+# for i in range(8):
+# 	if len.c <= 2:
+# 		i = random.randomchoice(a)
+# 		c.append(i)
+# 	else:
+# 		for i in range(6)
+# 			i = random.randint(1,9)
+# 			c.append(i)
+# while True:
+# 	if len(c) < 2:
+# 		for i in a:
+# 			c.append(random.choice(i))
+# 	elif len(c) >= 2 and len(c) < 8:
+# 		c.append(random.randint(1,9))
+# 	else:
+# 		break
+# print(c)
+# res = ''
+# while True:
+# 	if len(res) < 2:
+# 		for i in a:
+# 			res += (random.choice(i))
+# 	elif len(res) >= 2 and len(res) < 8:
+# 		res += str(random.randint(1,9))
+# 	else:
+# 		break
+# print(res)
+# count = 0 
+# tveri = 0
+# while True:
+# 	pas = input('Введите пароль: ')
+# 	if len(pas) == 8:
+# 		for i in pas:
+# 			if i.isalpha():
+# 				count +=1
+# 			if i.isdigit():
+# 				tveri += 1
+# 	if tveri == 6:
+# 		print('Ваш папроль принят')
+# 		break
+# 	else:
+# 		('введите цифры 6')
+# 	print(tveri)	
+					
+	
+
+
+
+
